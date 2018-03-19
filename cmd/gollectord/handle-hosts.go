9c40966feceacc36d72f5e3e00c9163d751dc626
jiayaohuang <jiayaohuang@pptv.com>
2018-03-19 16:02:47 +0800

    add a simple page to show hosts

---
+++ cmd/gollectord/handle-hosts.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gileshuang/gollector/lib/model"
+)
+
+func handleHosts(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	if r.Method != http.MethodGet && r.Method != http.MethodPost {
+		t, _ := template.ParseFiles("template/rawtext.html")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		t.Execute(w, "Invalid http method.")
+		return
+	}
+
+	var (
+		res        map[string]*model.HostInfo
+		bSrchName  bool
+		bSrchKey   bool
+		bSrchValue bool
+	)
+	res = make(map[string]*model.HostInfo)
+	bSrchName = false
+	bSrchKey = false
+	bSrchValue = false
+
+	// Filter
+	keyWord := r.PostForm.Get("keyword")
+	for _, rField := range r.PostForm["field"] {
+		switch rField {
+		case "hostname":
+			bSrchName = true
+		case "key":
+			bSrchKey = true
+		case "value":
+			bSrchValue = true
+		}
+	}
+	for k, v := range allHostsInfo {
+		if bSrchName == true && strings.Contains(k, keyWord) {
+			res[k] = allHostsInfo[k]
+		} else {
+			for ki, vi := range v.Info {
+				if (bSrchKey == true && strings.Contains(ki, keyWord)) ||
+					(bSrchValue == true && strings.Contains(vi.Value, keyWord)) {
+					if _, ok := res[k]; !ok {
+						res[k] = new(model.HostInfo)
+						res[k].HostName = k
+						res[k].Info = make(map[string]*model.AtomInfo)
+					}
+					res[k].Info[ki] = allHostsInfo[k].Info[ki]
+				}
+			}
+		}
+	}
+
+	t, err := template.ParseFiles("template/hosts.html")
+	if err != nil {
+		log.Println("handleHosts.template.ParseFiles:", err)
+	}
+	t.Execute(w, res)
+	return
+}
